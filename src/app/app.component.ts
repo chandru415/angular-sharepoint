cd33c0280de687c251fa022ded4f0a65da43baa0
chandru415 <chandru9052@gmail.com>
2023-05-11 11:15:58 +0530

    [chandu] implemented the file upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GraphqlService } from './services/graphql/graphql.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-sharepoint';
+
+  constructor(private gqlService: GraphqlService) {}
 
   onFileChange(pFileList: File[]) {
     let fileReqCount = 0;
@@ -17,5 +21,29 @@ export class AppComponent {
         isConsistsGreaterSize = true;
       }
     });
+
+    if (!isConsistsGreaterSize) {
+      pFileList.forEach((file) => {
+        this.gqlService
+          .uploadToDocumentLibrary(
+            file,
+            new Date().toLocaleDateString().replace('/', '-').replace('/', '-')
+          )
+          .pipe(take(1))
+          .subscribe(
+            (res) => {
+              fileReqCount++;
+
+              if (fileReqCount === pFileList.length) {
+                /** display complete message */
+              } else {
+              }
+            },
+            (error) => {
+              console.error(error, 'file upload');
+            }
+          );
+      });
+    }
   }
 }
